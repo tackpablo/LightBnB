6faf6d702c373e54578e071b9881c18f9ea49eb2
Pablo Tack <tackpablo@gmail.com>
2022-03-08 22:15:40 -0800

    Added query for most visited cities

---
+++ 1_queries/4_most_visited_cities.sql
@@ -0,0 +1,4 @@
+SELECT properties.city, COUNT(reservations.*) AS total_reservations FROM properties
+JOIN reservations ON properties.id = reservations.property_id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
