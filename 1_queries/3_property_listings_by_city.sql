98b1b430451dab350313299d892c15cfa90ab35d
Pablo Tack <tackpablo@gmail.com>
2022-03-08 22:12:17 -0800

    Added query for property listings by city

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,18 @@
+SELECT properties.id, properties.title, properties.cost_per_night, AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE properties.city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
+
+-- Compass answer
+-- SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+-- FROM properties
+-- JOIN property_reviews ON properties.id = property_id
+-- WHERE city LIKE '%ancouv%'
+-- GROUP BY properties.id
+-- HAVING avg(property_reviews.rating) >= 4
+-- ORDER BY cost_per_night
+-- LIMIT 10;
