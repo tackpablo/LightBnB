00a256e04a827f16d4d0caeab632f966afcb51b3
Pablo Tack <tackpablo@gmail.com>
2022-03-10 08:00:03 -0800

    Add functionality to save new property to properties table

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -179,7 +179,7 @@ const getAllProperties = function (options, limit = 10) {
   }
 
   queryString += `
-  GROUP BY properties.id`; // required here as
+  GROUP BY properties.id`; // required here as GROUP BY needs to be before HAVING (if it populates at all)
 
   if (options.minimum_rating) {
     queryParams.push(`${options.minimum_rating}`);
@@ -193,8 +193,8 @@ const getAllProperties = function (options, limit = 10) {
     `;
 
   // 5
-  console.log("options :", options);
-  console.log(queryString, queryParams);
+  // console.log("options :", options);
+  // console.log(queryString, queryParams);
 
   // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
@@ -227,9 +227,39 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(
+      `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;
+    `,
+      [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms,
+        true,
+      ]
+    )
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      return console.log(err.message);
+    });
+
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
